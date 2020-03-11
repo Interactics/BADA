6392d49241249afea2b9599f26ccecc5f226af15
Interactics <zz31@naver.com>
2020-03-11 16:26:24 +0900

    delete rs_talker's remove Frame

--- SLAM/realsense/rs_node/scripts/rs_camera_pub.py
+++ SLAM/realsense/rs_node/scripts/rs_camera_pub.py
@@ -11,29 +11,28 @@ def talker() :
     rospy.loginfo('rs_camera_pub node started')
 
     pipeline = rs.pipeline()
-    cobridge = CvBridge() 
-    debridge = CvBridge()
+    color_bridge = CvBridge() 
+    depth_bridge = CvBridge()
 
-    #rs setting start
+    #RealSense Camera setting
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     profile = pipeline.start(config)
-
     depth_sensor = profile.get_device().first_depth_sensor()
     depth_scale = depth_sensor.get_depth_scale()
 
     #removing background setting
-    clipping_distance_in_meters = 2 #1 meter
-    clipping_distance = clipping_distance_in_meters / depth_scale
+    # clipping_distance_in_meters = 2 #1 meter
+    # clipping_distance = clipping_distance_in_meters / depth_scale
 
     #aligning image
     align_to = rs.stream.color
     align = rs.align(align_to)
 
     #publisher 
-    colorPub = rospy.Publisher('color_image', Image, queue_size =1)
-    depthPub = rospy.Publisher('depth_image', Image, queue_size =1)
+    colorPub = rospy.Publisher('color_image', Image, queue_size = 1)
+    depthPub = rospy.Publisher('depth_image', Image, queue_size = 1)
 
     # cv2.namedWindow("img", cv2.WINDOW_NORMAL)
     try:
@@ -43,7 +42,7 @@ def talker() :
             frames = pipeline.wait_for_frames()            
             aligned_frames = align.process(frames)
             
-            #type : class <pyrealsense2.pyrealsense2.video_frame>
+            ##type : class <pyrealsense2.pyrealsense2.video_frame>
             #aligned_depth_frame is a 640x480 depth image
             aligned_depth_frame = aligned_frames.get_depth_frame() 
             color_frame = aligned_frames.get_color_frame() 
@@ -51,29 +50,30 @@ def talker() :
             if not aligned_depth_frame or not color_frame:
                 continue
 
-            #type : numpy.ndarray
+            ##Chage Type => type : numpy.ndarray
             color_image = np.asanyarray(color_frame.get_data())
             depth_image = np.asanyarray(aligned_depth_frame.get_data())
 
-            #remove background
-            grey_color = 153
-            depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-            bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+            ##remove background
+            # grey_color = 153
+            # depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+            # bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
 
             
             #removed img
-            imgMsgCol = cobridge.cv2_to_imgmsg(bg_removed, encoding="bgr8")
-            #imgMsgCol = cobridge.cv2_to_imgmsg(color_image, encoding="bgr8")
-            imgMsgDep = debridge.cv2_to_imgmsg(depth_image, encoding="16UC1")
+            # imgMsgCol = color_bridge.cv2_to_imgmsg(bg_removed, encoding="bgr8")
+            imgMsgCol = color_bridge.cv2_to_imgmsg(color_image, encoding="bgr8")
+            imgMsgDep = depth_bridge.cv2_to_imgmsg(depth_image, encoding="16UC1")
 
+            ## Publish
             colorPub.publish(imgMsgCol)
             depthPub.publish(imgMsgDep)
 
-            # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-            # cv2.imshow("color_image", color_image)
-            # cv2.imshow('depth_colormap', depth_colormap)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            cv2.imshow("color_image", color_image)
+            cv2.imshow('depth_colormap', depth_colormap)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
             rospy.Rate(30).sleep()
             #^^ talker HZ
