04e1b4e5c354a9e11706793335d7314381caf20f
Jiwon-h <g1@DESKTOP-3388FRV.localdomain>
2020-05-18 21:38:56 +0900

    change data structure and Kakao API

--- Visualization/ros2d/public/index.js
+++ Visualization/ros2d/public/index.js
@@ -1,5 +1,4 @@
 
-
 var canvas;
 var ctx;
 var start;
@@ -9,25 +8,69 @@ var y;
 var theta;
 var data;
 var time;
+var sig_name;
+
 const today = new Date();
 today.setTime(0);
+
 const FRAMES_PER_SECOND = 10;  // Valid values are 60,30,20,15,10...
 const FRAME_MIN_TIME = (1000/60) * (60 / FRAMES_PER_SECOND) - (1000/60) * 0.5;
 var lastFrameTime = 0;  // the last frame time
-Kakao.init("b886eede39b9d47bc9d3cb6e91483799");   // 사용할 앱의 JavaScript 키를 설정
-/*
-//kakao API
-Kakao.init('b886eede39b9d47bc9d3cb6e91483799');
-console.log(Kakao.isInitialized());
-*/
 
-function shareKakaotalk(data) {
-  var n=data;
+
+//KAKAO_TOKEN="adwnM5UYyKBlm7Pg-9OC9BjoDwdUOUo8dCY9DgopyV4AAAFyI3fYdQ";
+Kakao.init("b886eede39b9d47bc9d3cb6e91483799");   // 사용할 앱의 JavaScript 키를 설정
+
+ 
+  // 카카오 로그인 버튼을 생성합니다. 
+ 
+
+    /*Kakao.Auth.login({
+	    scope: 'talk_message,friends',
+	    success: function(response) {
+	        console.log(response);
+	    },
+	    fail: function(error) {
+	        console.log(error);
+	    }
+  });*/
+  
+  function shareKakaotalk(sig_name)
+  {
+    Kakao.API.request({
+      url: '/v2/api/talk/memo/default/send',
+      data: {
+        template_object: {
+          object_type: 'text',
+          text: sig_name,
+          link: {
+              web_url: 'http://192.168.137.1',
+              mobile_web_url: 'http://192.168.137.1',
+            },
+        },
+      },
+      success: function(response) {
+        console.log(response);
+      },
+      fail: function(error) {
+        console.log(error);
+      },
+    });
+  
+  }
+  shareKakaotalk("BADA 인증 완료");
+
+
+
+/*
+리스트방식으로 카카오톡 나에게로 메시지 보내기
+function shareKakaotalk(sig_name) {
+  var n=sig_name;
   Kakao.Link.sendDefault({
         objectType:"feed"
       , content : {
             title:"이벤트 발생"   // 콘텐츠의 타이틀
-          , description: n/* 콘텐츠 상세설명*/   
+          , description: n/ //콘텐츠 상세설명
           , imageUrl:"/mnt/c/Users/giwon/Downloads/BADA.jpg"   // 썸네일 이미지 이거 링크로 바꿔야할듯
           , link : {
                 mobileWebUrl:"http://192.168.137.1"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
@@ -36,7 +79,14 @@ function shareKakaotalk(data) {
       }
   });
 }
+*/
 
+/*
+카카오 디벨로퍼스에 소개된 카카오톡 스크랩 메시지 나에게 보내는 샘플
+curl -v -X POST "https://kapi.kakao.com/v2/api/talk/memo/scrap/send" \
+    -H "Authorization: Bearer t5lSxCO7Jilh9uIVAlL4sQLwUboITRLkpw8RmAo9cusAAAFyJ12yxA" \
+    -d 'request_url=http://localhost:3000'
+*/
 
 
 //Tab design 
@@ -55,24 +105,25 @@ $(document).ready(function(){
   });
 });
 
-class List{
-  constructor()
-  {
-    this.listSize=0;
-    this.pos=0;
-    this.dataStore=[];
-  } 
+function Queue(){
 
-  append(element){
-    this.dataStore[this.listSize++]=element;
-  }
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.search=search;
+}
 
-  length()
-  {
-    return this.listSize;
-  }
+function enqueue(element)
+{
+    this.dataStore.push(element);
+}
 
-  search(){
+function dequeue()
+{
+    return this.dataStore.shift();
+}
+
+function search(){
     var cnt=0;
     for(var i=0; i<this.dataStore.length;i++)
     {
@@ -82,11 +133,11 @@ class List{
       }
       else{
         cnt++;
-        console.log("cnt++");
+        console.log("Water events occur" + cnt + "times");
       }
     }
     if(cnt>=3)
-    { console.log("cnt>=3");
+    { console.log("Water event exceed 3 times. Send Message");
       return true;
     }
     else{
@@ -94,21 +145,12 @@ class List{
     }
   }
 
-  remove(){
-    var pos=this.listSize-1;
-    this.dataStore.splice(pos,1);
-    --this.listSize;
-  }
-}
-
-//overflow 막기위해서 10분에한번씩 water.remove실행
 w_remove = setInterval(function() {
-  if(water.listSize>0){
-    water.remove();}
+    water.dequeue();
 }, 600000);
 
+var water= new Queue();
 
-var water = new List();
 
 function tryConnectWebsocket(){
 
@@ -194,27 +236,28 @@ function tryConnectWebsocket(){
   });
 
   signal.subscribe(function(m){
-    data=m.data;
+    sig_name=m.data;
+    console.log("NOW SIGNAL : "+sig_name);
     time=today.getTime();
-    if(data=='Water')
+    if(sig_name=='Water')
     {
-      //먼저 검색해
+      //먼저 검색해 
       if(water.search())
-      {
-        console.log("발생한적있어.");
-      //발생한적있다면
-      shareKakaotalk(data);
+      { 
+        //총 3번 이상 발생했다면
+        shareKakaotalk(sig_name);
       }
       else
       {
-      //발생한적없다면
-      water.append(time);
+       //발생한적없다면
+       water.enqueue(time);
       }
-  
     }
-  }
-  );
-  
+    else if(sig_name!="Silence")
+    {
+        shareKakaotalk(sig_name);
+    }
+  });
 }
 
 
