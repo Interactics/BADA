5eb800cff160fa011a0896a9304b3ee71f4163ed
Jiwon-h <g1@DESKTOP-3388FRV.localdomain>
2020-05-23 19:03:43 +0900

    Add PrintHistory

--- Visualization/ros2d/public/index.js
+++ Visualization/ros2d/public/index.js
@@ -9,35 +9,22 @@ var str;
 var data;
 var time;
 var sig_name;
+var viewtime;
+var PrintHistory="Connection";
 
 const today = new Date();
 today.setTime(0);
 
+var hidx=0;
 const FRAMES_PER_SECOND = 10;  // Valid values are 60,30,20,15,10...
 const FRAME_MIN_TIME = (1000 / 60) * (60 / FRAMES_PER_SECOND) - (1000 / 60) * 0.5;
 var lastFrameTime = 0;  // the last frame time
 
-//Tab design 왜 안되냐..jquery 동작안해, html에서 jquery 소스 불러왔는데도 연결 안되는듯.
 
-//KAKAO_TOKEN="adwnM5UYyKBlm7Pg-9OC9BjoDwdUOUo8dCY9DgopyV4AAAFyI3fYdQ";
 Kakao.init("b886eede39b9d47bc9d3cb6e91483799");   // 사용할 앱의 JavaScript 키를 설정
 
- 
-  // 카카오 로그인 버튼을 생성합니다. 
- 
-
-    /*Kakao.Auth.login({
-	    scope: 'talk_message,friends',
-	    success: function(response) {
-	        console.log(response);
-	    },
-	    fail: function(error) {
-	        console.log(error);
-	    }
-  });*/
-  
-  function shareKakaotalk(sig_name)
-  {
+function shareKakaotalk(sig_name)
+{
     Kakao.API.request({
       url: '/v2/api/talk/memo/default/send',
       data: {
@@ -57,37 +44,36 @@ Kakao.init("b886eede39b9d47bc9d3cb6e91483799");   // 사용할 앱의 JavaScript
         console.log(error);
       },
     });
-  
   }
   shareKakaotalk("BADA 인증 완료");
 
 
 
-/*
-리스트방식으로 카카오톡 나에게로 메시지 보내기
-function shareKakaotalk(sig_name) {
-  var n=sig_name;
-  Kakao.Link.sendDefault({
-        objectType:"feed"
-      , content : {
-            title:"이벤트 발생"   // 콘텐츠의 타이틀
-          , description: n/ //콘텐츠 상세설명
-          , imageUrl:"/mnt/c/Users/giwon/Downloads/BADA.jpg"   // 썸네일 이미지 이거 링크로 바꿔야할듯
-          , link : {
-                mobileWebUrl:"http://192.168.137.1"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
-              , webUrl:"http://192.168.137.1" // PC버전 카카오톡에서 사용하는 웹 링크 URL
-          }
-      }
-  });
-}
-*/
 
-/*
-카카오 디벨로퍼스에 소개된 카카오톡 스크랩 메시지 나에게 보내는 샘플
-curl -v -X POST "https://kapi.kakao.com/v2/api/talk/memo/scrap/send" \
-    -H "Authorization: Bearer t5lSxCO7Jilh9uIVAlL4sQLwUboITRLkpw8RmAo9cusAAAFyJ12yxA" \
-    -d 'request_url=http://localhost:3000'
-*/
+  function printNow() {
+    const today = new Date();
+  
+    // getDay: 해당 요일(0 ~ 6)를 나타내는 정수를 반환한다.
+  
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+  
+    // 12시간제로 변경
+    hour %= 12;
+    hour = hour || 12; // 0 => 12
+  
+    // 10미만인 분과 초를 2자리로 변경
+    minute = minute < 10 ? '0' + minute : minute;
+    second = second < 10 ? '0' + second : second;
+  
+    var now = `${year}.${month}.${date}    ${ampm} ${hour}:${minute}:${second} `;
+    return now;
+  };
 
 
 function Queue(){
@@ -96,6 +82,7 @@ function Queue(){
     this.enqueue = enqueue;
     this.dequeue = dequeue;
     this.search=search;
+    this.toString=toString;
 }
 
 function enqueue(element)
@@ -104,9 +91,10 @@ function enqueue(element)
 }
 
 function dequeue()
-{
+{   
     return this.dataStore.shift();
 }
+
 
 var cnt=0;
 function search(){
@@ -130,12 +118,22 @@ function search(){
       return false;
     }
   }
+  
+  function toString() {
+    var retStr = "";
+    for (var i = this.dataStore.length-1;i >=0; --i )    {
+        retStr += this.dataStore[i] + "\n";
+    }
+    return retStr;
+}
 
-w_remove = setInterval(function() {
-    water.dequeue();
-}, 600000);
+
 
 var water= new Queue();
+
+var h_element;
+var h = new Queue();
+
 
 
 function tryConnectWebsocket() {
@@ -146,31 +144,20 @@ function tryConnectWebsocket() {
 
   // If there is an error on the backend, an 'error' emit will be emitted.
   ros.on('error', function (error) {
-    // document.getElementById('connecting').style.display = 'none';
-    // document.getElementById('connected').style.display = 'none';
-    // document.getElementById('closed').style.display = 'none';
-    // document.getElementById('error').style.display = 'inline';
     console.log(error);
   });
 
   // Find out exactly when we made a connection.
   ros.on('connection', function () {
     console.log('Connection made!');
-    // document.getElementById('connecting').style.display = 'none';
-    // document.getElementById('error').style.display = 'none';
-    // document.getElementById('closed').style.display = 'none';
-    // document.getElementById('connected').style.display = 'inline';
   });
 
   ros.on('close', function () {
     console.log('Connection closed.');
-    // document.getElementById('connecting').style.display = 'none';
-    // document.getElementById('connected').style.display = 'none';
-    // document.getElementById('closed').style.display = 'inline';
   });
 
   // Create a connection to the rosbridge WebSocket server.
-  ros.connect('ws://192.168.10.105:9090');
+  ros.connect('ws://192.168.0.6:9090');
 
   // Like when publishing a topic, we first create a Topic object with details of the topic's name
   // and message type. Note that we can call publish or subscribe on the same topic object.
@@ -220,16 +207,21 @@ function tryConnectWebsocket() {
     name : '/signal',
     messageType : 'std_msgs/String'
   });
-
+  
   signal.subscribe(function(m){
     sig_name=m.data;
-
-
     console.log("NOW SIGNAL : "+sig_name);
     document.getElementById("signal").innerHTML = sig_name;
+
+
     time=today.getTime();
+    viewtime=printNow();
 
-
+    if(h.dataStore.length>=5)
+    {
+      h.dequeue();
+    }
+  
     if(sig_name=='Water')
     {
       //먼저 검색해 
@@ -240,9 +232,11 @@ function tryConnectWebsocket() {
         {
           water.dequeue();
         }
-      
         cnt=0;
         shareKakaotalk(sig_name);
+        h.enqueue([sig_name, viewtime]);
+        hidx=hidx+1;     
+        PrintHistory=h.toString();
       }
       else
       {
@@ -253,9 +247,16 @@ function tryConnectWebsocket() {
     else if(sig_name!="Silence")
     {
         shareKakaotalk(sig_name);
+        h.enqueue([sig_name, viewtime]);
+        hidx=hidx+1;       
+        PrintHistory=h.toString();
     }
+    document.getElementById("History").innerHTML=PrintHistory;
+
   });
+
 }
+
 
 
 
